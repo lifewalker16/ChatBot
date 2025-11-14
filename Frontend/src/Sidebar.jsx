414bb91dfc48ef6688f81e415b8f3cb5a208196e
sunny cardoso <sunnycardoso750@gmail.com>
2025-11-14 12:43:51 +0530

    Fix image path

--- Frontend/src/Sidebar.jsx
+++ Frontend/src/Sidebar.jsx
@@ -68,7 +68,7 @@ function Sidebar(){
     return(
       <section className="sidebar">
          <button onClick={createNewChat}>
-          <img src="src/assets/blacklogo.png" alt="gptlogo" className="logo"></img>
+          <img src="/blacklogo.png" alt="gptlogo" className="logo"></img>
           <span><i className="fa-solid fa-pen-to-square"></i></span>
          </button>
 
